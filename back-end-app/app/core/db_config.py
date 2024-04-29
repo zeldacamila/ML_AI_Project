7acafb5daab00818dd07654d7f5476df64479fea
Mateo Paloma <matepalqui@hotmail.com>
2024-04-29 15:21:54 -0500

    ci: env for test add

--- back-end-app/app/core/db_config.py
+++ back-end-app/app/core/db_config.py
@@ -6,7 +6,7 @@ import os
 DB_USERNAME = os.environ.get("DB_USERNAME")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
 DB_HOST = os.environ.get("DB_HOST")
-DB_NAME = os.environ.get("mlproject")
+DB_NAME = os.environ.get("DB_NAME")
 
 DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 # DATABASE_URL = "postgresql://postgres:123456@localhost/postgres"
