7c373e3fe6d74128a56cc6884359d4720dcb3f55
Maria C Recuero <zeldacamila0913@gmail.com>
2024-04-29 13:45:39 -0500

    (ci): configured dockerfile and setup CI

--- back-end-app/app/core/db_config.py
+++ back-end-app/app/core/db_config.py
@@ -1,8 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm.session import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost/postgres"
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 # DATABASE_URL = "postgresql://postgres:123456@localhost/postgres"
 
 
