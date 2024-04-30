4c10ec829eed7efd9391cdd2833180c2b0f1aa65
Maria C Recuero <zeldacamila0913@gmail.com>
2024-04-30 09:34:13 -0500

    feat(user): fixed model import

--- back-end-app/app/core/db_config.py
+++ back-end-app/app/core/db_config.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.session import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:123456@localhost/postgres"
+DATABASE_URL = "postgresql://postgres:postgres@localhost/postgres"
+# DATABASE_URL = "postgresql://postgres:123456@localhost/postgres"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
