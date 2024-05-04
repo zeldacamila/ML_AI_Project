9d517b8898d4f37c11e6c1540ad50fc3f6dc41fa
Maria C Recuero <zeldacamila0913@gmail.com>
2024-05-04 11:47:25 -0500

    test: Updated boardGamesApi's baseURL

--- front-end-app/src/api/boardGamesApi.ts
+++ front-end-app/src/api/boardGamesApi.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 //* Configurando axios para realizar peticiones.
 const boardGamesApi = axios.create({
-    //baseURL: "http://127.0.0.1:8000",
-    baseURL: "http://15.228.163.179:2000",
+  //baseURL: "http://127.0.0.1:8000",
+  baseURL: "http://15.228.156.203:2000",
 });
 
 export default boardGamesApi;
