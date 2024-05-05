e4a591835e664d41e74be759051ca20ad26575b0
Maria C Recuero <zeldacamila0913@gmail.com>
2024-05-05 18:19:51 -0500

    fix: Fix UUID generation in User model to ensure uniqueness. Fix timestamp generation in Recommendation model.
    Updated the default method for the creation_date in the Recommendation model to use `datetime.now` without parentheses. This ensures the correct timestamp is generated at the time each instance is created.
    
    Changed the default ID generation for the User model to use a lambda function. This ensures a new UUID is generated for each User instance, preventing duplicate ID errors.

--- back-end-app/app/src/models.py
+++ back-end-app/app/src/models.py
@@ -11,7 +11,7 @@ from app.core.db_config import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(String, primary_key=True, default=str(uuid4()))
+    id = Column(String, primary_key=True, default=lambda: str(uuid4()))
     username = Column(String(25), unique=True, nullable=False)
     email = Column(String(50), unique=True, nullable=False)
     password = Column(String, nullable=False)
@@ -25,7 +25,7 @@ class Recommendation(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String(50), nullable=False)
     description = Column(String, nullable=False)
-    creation_date = Column(DateTime, default=datetime.now())
+    creation_date = Column(DateTime, default=datetime.now)
     user_id = Column(String, ForeignKey("users.id"))
 
     user = relationship("User", back_populates="recommendations")
